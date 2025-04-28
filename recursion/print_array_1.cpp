6c551a13bfea316477b0249a687303319e8268d2
amandhakad13 <ad5538694@gmail.com>
2025-04-27 18:10:59 -0700

    print array method 1

---
+++ recursion/print_array_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int index, int n) {
+
+    if(index == n)
+        return;
+
+    cout<<arr[index]<<" ";
+    printArray(arr, index+1, n);
+}
+
+int main() {
+    int arr[20];
+    int n;
+
+    cout<<"Enter the size max(20) : ";
+    cin>>n;
+    cout<<"Enter the elements : ";
+    for(int i = 0; i<n; i++) {
+        cin>>arr[i];
+    }
+    printArray(arr, 0, n);
+}
