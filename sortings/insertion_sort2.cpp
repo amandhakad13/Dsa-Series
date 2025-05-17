be86869e275385d3f264e60d0537e81c708ced34
amandhakad13 <ad5538694@gmail.com>
2025-05-16 22:23:32 -0700

    insertion sort program 2

---
+++ sortings/insertion_sort2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+
+    int n;
+    cout<<"Enter the size : ";
+    cin>>n;
+
+    cout<<"Enter the elements : ";
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    cout<<"Before sorting : ";
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+    for(int i=1; i<n; i++) {
+        for(int j=i; j>0; j--) {
+            if(arr[j]>arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    cout<<"\nAfter sorting : ";
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+}
