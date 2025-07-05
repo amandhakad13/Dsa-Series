456ae69fa4a48b944d3bb238d3d229b0291687c9
amandhakad13 <ad5538694@gmail.com>
2025-07-04 21:33:35 -0700

    insertion at end program 1

---
+++ linked list practice programs/insertion_at_end_program1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class NODE {
+    public :
+        int data;
+        NODE *next;
+
+        NODE(int value) {
+            data = value;
+            next = NULL;
+        }
+};
+
+int main() {
+
+    NODE *Head, *Tail;
+    Head = NULL;
+
+    int arr[] = {2,4,6,8,10};
+
+    for(int i=0; i<5; i++) {
+        if(Head == NULL) {
+            Head = new NODE(arr[i]);
+            Tail = Head;
+        }
+        else {
+            while(Tail->next!=NULL) {
+                Tail = Tail->next;
+            }
+            NODE *temp;
+            temp = new NODE(arr[i]);
+            Tail->next = temp;
+        }
+    }
+
+    NODE *temp;
+    temp = Head;
+    while(temp!=NULL) {
+        cout<<temp->data;
+        if(temp->next!=NULL) {
+            cout<<" -> ";
+        }
+        temp=temp->next;
+    }
+    
+}
